1a6067e14a09c7715924be1d0362c2249c9ce55f
seiyo1 <seiyo.03oo@gmail.com>
2026-01-02 04:29:06 +0900

    Accept book path as command-line argument

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_num_words, get_num_chars, num_chars_to_sorted_list
 
 def get_book_text(path):
@@ -6,9 +7,14 @@ def get_book_text(path):
     return content
 
 def main():
-    content = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    path = sys.argv[1]
+    content = get_book_text(path)
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {path}...")
     print("----------- Word Count ----------")
     print(f"Found {get_num_words(content)} total words")
     counts = get_num_chars(content)
