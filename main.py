62f6b238c0cf093b51b49097051e913cc2d8d098
seiyo1 <seiyo.03oo@gmail.com>
2026-01-02 02:56:43 +0900

    Refactor

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
+from stats import get_num_words
+
 def get_book_text(path):
     with open(path, "r", encoding="utf-8") as f:
         content = f.read()
     return content
 
 
-def count_words_number(text):
-    word_list = text.split()
-    return len(word_list)
-
-
 def main():
     text = get_book_text("books/frankenstein.txt")
-    num = count_words_number(text)
+    num = get_num_words(text)
     print(f"Found {num} total words")
           
 if __name__ == "__main__":
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_num_words(text):
+    word_list = text.split()
+    return len(word_list)
