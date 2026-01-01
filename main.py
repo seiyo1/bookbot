d3d3505a6cbcce91aed64298e66d9938d8863379
seiyo1 <seiyo.03oo@gmail.com>
2026-01-02 02:52:59 +0900

    Count words in book text

--- main.py
+++ main.py
@@ -4,9 +4,15 @@ def get_book_text(path):
     return content
 
 
+def count_words_number(text):
+    word_list = text.split()
+    return len(word_list)
+
+
 def main():
-    ret = get_book_text("books/frankenstein.txt")
-    print(ret)
-    
+    text = get_book_text("books/frankenstein.txt")
+    num = count_words_number(text)
+    print(f"Found {num} total words")
+          
 if __name__ == "__main__":
     main()
