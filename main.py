3d7651f46cc2b9ec4de20ca65119a0abc70d6190
seiyo1 <seiyo.03oo@gmail.com>
2026-01-02 04:19:39 +0900

    Add sorted character count display feature

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_chars, num_chars_to_sorted_list
 
 def get_book_text(path):
     with open(path, "r", encoding="utf-8") as f:
         content = f.read()
     return content
 
-
 def main():
-    text = get_book_text("books/frankenstein.txt")
-    num = get_num_words(text)
-    print(f"Found {num} total words")
-          
+    content = get_book_text("books/frankenstein.txt")
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {get_num_words(content)} total words")
+    counts = get_num_chars(content)
+    for c in num_chars_to_sorted_list(counts):
+        print(f"{c['char']}: {c['num']}")
+    print("============= END ===============")
+
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -1,3 +1,24 @@
 def get_num_words(text):
     word_list = text.split()
     return len(word_list)
+
+
+def get_num_chars(text):
+    counts = {}
+    word_list = text.split()
+    for word in word_list:
+        for char in word.lower():
+            if char.isalpha(): 
+                counts[char] = counts.get(char, 0) + 1
+    return counts
+
+
+def num_chars_to_sorted_list(counts):
+    sorted_list = []
+    for key, value in counts.items():
+        sorted_list.append({"char" : key, "num": value})
+    def sort_on(dict):
+        return dict["num"]
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+    
